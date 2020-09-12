52aaed5a5698d5f7da5586e06454c18b128ee89e
eki66 <soshieki@gmail.com>
2020-09-13 01:24:01 +0800

    add check file exist

--- products.py
+++ products.py
@@ -1,12 +1,18 @@
+import os # operating system 
+
 # 讀取檔案
 products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if ('商品,價格') in line:
-			continue #繼續,不往下執行或跳出迴圈break,而是直接處理下一迴
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'): # 檢查檔案在不在
+	print('yeah! 找到檔案了!')
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if ('商品,價格') in line:
+				continue #繼續,不往下執行或跳出迴圈break,而是直接處理下一迴
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('找不到檔案...')
 
 # 讓使用者輸入
 while True:
@@ -22,7 +28,7 @@ print(products)
 for p in products:
 	print(p[0], '的價格是', p[1])
 
-# 寫入檔案
+# 寫入檔案,如果沒有products.csv,則會建立!
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
